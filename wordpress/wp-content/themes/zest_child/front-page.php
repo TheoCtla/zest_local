ebbc8de6a56a76e67fd308a6d6be95d2cc105933
TheoCtla <tcatala32@gmail.com>
2025-04-16 14:48:26 +0200

    wordpress + theme installé, on peut commencer le dev

---
+++ wordpress/wp-content/themes/zest_child/front-page.php
@@ -0,0 +1,20 @@
+<?php
+get_header(); // Appelle le fichier header.php
+?>
+
+<main id="main" class="site-main">
+
+  <section class="hero">
+    <div class="container">
+      <h1>Une nouvelle façon de briller sur le web</h1>
+      <p>Un site moderne, rapide et 100% personnalisé</p>
+      <a href="#contact" class="cta-btn">Je découvre</a>
+    </div>
+  </section>
+
+</main>
+
+<?php
+get_footer(); // Appelle le fichier footer.php
+?>
+
